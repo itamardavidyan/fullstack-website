55dce60d5d90645fbeeabfac4e656cfbf4da9eb5
Itamar Davidyan <itamardavidyan@gmail.com>
2018-10-02 11:14:23 +0300

    upload to Heroku

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ const app = express();
 var bodyParser = require('body-parser');
 
 const MongoClient = require('mongodb').MongoClient;
-// const mongoDBurl = "mongodb://<dbuser>:<dbpassword>@ds161112.mlab.com:61112/alerts";
-const mongoDBurl = "mongodb://admin:myFirstWebinar1@ds161112.mlab.com:61112/alerts";
+// const mongoDBurl = "mongodb://<dbuser>:<dbpassword>@ds259742.mlab.com:59742/alerts";
+const mongoDBurl = "mongodb://admin:myFirstWebinar1@ds259742.mlab.com:59742/alerts";
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', function(req,res) {
-	console.log("/");
-	res.render('index.html');
-});
 
 app.post('/show', function(req, res) {
 	console.log('/show');
@@ -23,11 +18,33 @@ app.post('/show', function(req, res) {
 	if (name === '' || name === undefined || name === null) return res.json({"msg":"enter your name!"});
 
 	// enter mondodb code
+	MongoClient.connect(mongoDBurl, { useNewUrlParser: true }, function(err, db) {
+		if (err) throw err;
 
+		const dbo = db.db("alerts");
+
+		dbo.collection("messages").countDocuments({}, function(err, numOfDocs){
+			if (err) throw err;
+			const ranMsgId = Math.floor(Math.random() * numOfDocs) + 1; // random number between 1 to numOfDocs
+
+			dbo.collection("messages").findOne({id:ranMsgId}, function(err, result) {
+				if (err) throw err;
+
+				var myquery = { id: ranMsgId };
+				var newvalues = { $inc: { counter: 1} };
+
+				dbo.collection("messages").updateOne(myquery, newvalues, function(err, unUseResult) {
+					if (err) throw err;
+					console.log(unUseResult);
+					return res.json({"msg":result.msg});
+				});
+			});
+		});
+	});
 
 	// <!-- 0 - comment when connect to mLab --!>
-	const msg = 'Hello ' + name + '!!';
-	return res.json({"msg":msg});
+	// const msg = 'Hello ' + name + '!!';
+	// return res.json({"msg":msg});
 
 });
 
@@ -35,13 +52,36 @@ app.post('/add', function(req, res) {
 	console.log('/add');
 
 	// enter mondodb code
+	var name = req.body.name;
+
+	if (name === '' || name === undefined || name === null) return res.json({"msg":"enter your name!"});
+
+	MongoClient.connect(mongoDBurl, { useNewUrlParser: true }, function(err, db) {
+		if (err) throw err;
+
+		var dbo = db.db("alerts");
+
+		dbo.collection("messages").countDocuments({}, function(err, numOfDocs){
+			if (err) throw err;
+			numOfDocs++;
+			console.log(numOfDocs);
+			const newMsg = 'Hello ' + name +'!';
+
+			var myobj = { id: numOfDocs , msg: newMsg, counter:0 };
+
+			dbo.collection("messages").insertOne(myobj, function(err, resp) {
+				if (err) throw err;
+				return res.json({"msg":'the alert created successfully'});
+			});
+		});
+	});
 
 });
 
 // <!-- 1 - simple connection to mongoDB --!>
 // { <-- show event -->
 
-	// MongoClient.connect(mongoDBurl, function(err, db) {
+	// MongoClient.connect(mongoDBurl, { useNewUrlParser: true }, function(err, db) {
 	// 	if (err) throw err;
 	// 	var dbo = db.db("alerts");
 	// 	dbo.collection("messages").findOne({id:1}, function(err, result) {
@@ -59,13 +99,12 @@ app.post('/add', function(req, res) {
 	// MongoClient.connect(mongoDBurl, { useNewUrlParser: true }, function(err, db) {
 	// 	if (err) throw err;
 
-	// 	var ranMsgId;
-	// 	var dbo = db.db("alerts");
+	// 	const dbo = db.db("alerts");
 
-    //     dbo.collection("messages").countDocuments({}, function(err, numOfDocs){
+	// 	dbo.collection("messages").countDocuments({}, function(err, numOfDocs){
 	// 		if (err) throw err;
 	// 		console.log(numOfDocs);
-	// 		ranMsgId = Math.floor(Math.random() * numOfDocs) + 1;
+	// 		const ranMsgId = Math.floor(Math.random() * numOfDocs) + 1;
 	// 		console.log(ranMsgId);
 
 	// 		dbo.collection("messages").findOne({id:ranMsgId}, function(err, result) {
@@ -73,7 +112,7 @@ app.post('/add', function(req, res) {
 	// 			return res.json({"msg":result.msg});
 	// 		});
 	// 	});
-    // });
+	// });
 
 // }
 
